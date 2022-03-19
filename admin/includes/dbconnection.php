4baaed261f593ac3c35267778ba8cd5d2d784f98
Jasmeetsaini801 <81513734+Jasmeetsaini801@users.noreply.github.com>
2022-03-19 07:39:15 +0530

    Add files via upload

---
+++ admin/includes/dbconnection.php
@@ -0,0 +1,14 @@
+<?php
+
+$con=mysqli_connect("bbnhzmnd9vixvttrvs7a-mysql.services.clever-cloud.com", "u8czkvduxmozyvyy", "mML2yH63VXRCQGUPspTH", "bbnhzmnd9vixvttrvs7a");
+
+if(mysqli_connect_errno()){
+
+echo "Connection Fail".mysqli_connect_error();
+
+}
+
+
+
+  ?>
+
